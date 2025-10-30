29b28918f985872472c883fb2bd4c7cfd067eae9
eleonorakuteva <eleonorakuteva@gmail.com>
2025-10-30 13:51:08 +0200

    Update adoption_center.py

---
+++ pet_adoption_center/adoption_center.py
@@ -0,0 +1,16 @@
+class AdoptionCenter:
+
+    def __init__(self, name:str, list_of_pets = None):
+        self.name = name
+        self.list_of_pets = list_of_pets if list_of_pets is not None else []
+
+    def __repr__(self):
+        return (f"{self.name} is a adoption center, where you can adopt your future pet!\n"
+                f"{self.list_available_pets()} ")
+
+    def list_available_pets(self):
+
+        available_pets = [f"{pet}" for pet in self.list_of_pets]
+
+        return (f"Available pets you can adopt:\n-> "
+                f"{"\n-> ".join(available_pets)}")
