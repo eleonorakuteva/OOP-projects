d57a80c82b293230cd95a1fc2aaab7421ae782ab
eleonorakuteva <eleonorakuteva@gmail.com>
2025-10-30 13:51:12 +0200

    Update pet_adoption_center.py

--- pet_adoption_center/pet_adoption_center.py
+++ pet_adoption_center/pet_adoption_center.py
@@ -7,22 +7,6 @@ Staff (workers) can add or remove pets from the center.
 """
 
 
-class AdoptionCenter:
-
-    def __init__(self, name:str, list_of_pets = None):
-        self.name = name
-        self.list_of_pets = list_of_pets if list_of_pets is not None else []
-
-    def __repr__(self):
-        return (f"{self.name} is a adoption center, where you can adopt your future pet!\n"
-                f"{self.list_available_pets()} ")
-
-    def list_available_pets(self):
-
-        available_pets = [f"{pet}" for pet in self.list_of_pets]
-
-        return (f"Available pets you can adopt:\n-> "
-                f"{"\n-> ".join(available_pets)}")
 
 
 class Pet:
