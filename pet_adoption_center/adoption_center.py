abc1ca2f944abbf4890ab50d6d29efa381066aac
eleonorakuteva <eleonorakuteva@gmail.com>
2025-10-30 15:11:11 +0200

    Update adoption_center.py

--- pet_adoption_center/adoption_center.py
+++ pet_adoption_center/adoption_center.py
@@ -11,8 +11,14 @@ class AdoptionCenter:
                 f"{self.list_available_pets()} ")
 
     def list_available_pets(self):
-
         available_pets = [f"{pet}" for pet in self.list_of_pets]
-
         return (f"Available pets you can adopt:\n-> "
                 f"{"\n-> ".join(available_pets)}")
+
+
+    def search_by_species(self, species_key):
+        pets_with_curr_species = [f"{pet.name} - {pet.age} years old." for pet in self.list_of_pets if pet.species == species_key]
+        if pets_with_curr_species:
+            return f"In adoption center {self.name} we have {len(pets_with_curr_species)} animals with species: {species_key}."+ '\n -> ' +'\n -> '.join(pets_with_curr_species)
+        return f"In adoption center {self.name} currently we don't have animals with species: {species_key}."
+
