d8e2f3cf54542ccb02c75015908766b0de8da4aa
eleonorakuteva <eleonorakuteva@gmail.com>
2025-10-30 13:54:53 +0200

    Update pet_adoption_center.py

--- pet_adoption_center/pet_adoption_center.py
+++ pet_adoption_center/pet_adoption_center.py
@@ -7,27 +7,6 @@ Staff (workers) can add or remove pets from the center.
 """
 
 
-
-
-class Pet:
-    def __init__(self, name:str, species:str, age:int):
-        self.name = name
-        self.species = species
-        self.age = age
-        self.is_adopted = False
-
-    def pet_dict(self):
-        return {self.species : {"name" : self.name,
-                                "age" : self.age,}
-                }
-
-    def search_by_species(self, species_key):
-        matches = {species_key for species_key, value in self.pet_dict()}
-        return matches
-
-    def __repr__(self):
-        return f"{self.name}, the {self.species}, age {self.age}."
-
 class Adopter:
     def __init__(self, name:str, adopted_pets_by_user = None):
         self.name = name
