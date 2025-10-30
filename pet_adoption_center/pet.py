182a24e8095a6146418e3fb2c8aee101ed765677
eleonorakuteva <eleonorakuteva@gmail.com>
2025-10-30 15:11:27 +0200

    Update pet.py

--- pet_adoption_center/pet.py
+++ pet_adoption_center/pet.py
@@ -1,18 +1,22 @@
+
+
+
 class Pet:
+    all_pets:dict[str,{str, bool|int|str}] = {}
+
     def __init__(self, name:str, species:str, age:int):
         self.name = name
         self.species = species
         self.age = age
         self.is_adopted = False
 
-    def pet_dict(self):
-        return {self.species : {"name" : self.name,
-                                "age" : self.age,}
-                }
+        # Add this pet to the shared class-level dictionary
+        Pet.all_pets.setdefault(species, []).append({
+            "name": name,
+            "age": age,
+            "is_adopted": self.is_adopted
+        })
 
-    def search_by_species(self, species_key):
-        matches = [value for species, value in self.pet_dict() if species == species_key]
-        return matches
 
     def __repr__(self):
         return f"{self.name}, the {self.species}, age {self.age}."
