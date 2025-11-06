1b68c85b046c14fe4f2addf962a9550e5bf88b8a
eleonorakuteva <eleonorakuteva@gmail.com>
2025-11-06 20:06:17 +0200

    Update menu_item.py

---
+++ restaurant_management/menu_item.py
@@ -0,0 +1,41 @@
+class MenuItem:
+    """
+    Represents a single item (e.g., Pizza, Coke, Tiramisu).
+    Stores name, category, price.
+    """
+    def __init__(self, name:str, category:str, price:float):
+        self.name = name
+        self.category = category
+        self.price = price
+
+    @property
+    def name(self):
+        return self.__name
+
+    @name.setter
+    def name(self, value):
+        if value == "":
+            raise ValueError("The item name cannot be an empty string")
+        self.__name = value
+
+        
+    @property
+    def category(self):
+        return self.__category
+
+    @category.setter
+    def category(self, value):
+        if value == "":
+            raise ValueError("The category cannot be an empty string")
+        self.__category = value
+
+
+    @property
+    def price(self):
+        return self.__price
+
+    @price.setter
+    def price(self, value):
+        if value <= 0.0:
+            raise ValueError("The price cannot be zero or less than zero.")
+        self.__price = value
