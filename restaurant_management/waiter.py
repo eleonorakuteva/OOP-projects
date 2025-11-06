65c57049f4321dcfeabc46e18f2ad89430005990
eleonorakuteva <eleonorakuteva@gmail.com>
2025-11-06 19:55:25 +0200

    Update waiter.py

--- restaurant_management/waiter.py
+++ restaurant_management/waiter.py
@@ -3,6 +3,16 @@ class Waiter:
         self.name = name
         self.id = id_
 
+    @property
+    def name(self):
+        return self.__name
+
+    @name.setter
+    def name(self, value):
+        if value == "":
+            raise ValueError("The waiter name cannot be an empty string")
+        self.__name = value
+
 
     @property
     def id(self):
@@ -10,5 +20,8 @@ class Waiter:
 
     @id.setter
     def id(self, value):
-        if value.isdigit() and len(value) == 10:
+        if value.isdigit() and len(value) == 8:
             self.__id = value
+        else:
+            raise ValueError("The id must contain 8 digits.")
+
