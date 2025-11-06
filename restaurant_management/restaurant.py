734d9d146193d19cd7337f7ba2d4652bb439810f
eleonorakuteva <eleonorakuteva@gmail.com>
2025-11-06 19:47:16 +0200

    Update restaurant.py

--- restaurant_management/restaurant.py
+++ restaurant_management/restaurant.py
@@ -9,8 +9,14 @@ class Restaurant:
         self.__list_of_waiters: list[Waiter] = []
         self.total_earnings = 0
 
-    def add_waiter(self):
-        pass
+    def add_customer(self, customer:Customer):
+        try:
+            self.__list_of_customers.append(customer)
+        except AttributeError:
+            return f"The {type(customer).__name__.lower()} {customer.name} already work in {self.name}"
 
-    def add_customer(self):
-        pass
+    def add_waiter(self, waiter:Waiter):
+        try:
+            self.__list_of_waiters.append(waiter)
+        except AttributeError:
+            return f"The {type(waiter).__name__.lower()} {waiter.name} already work in {self.name}"
