e9fec89c10bf2369068dcea487eaf032f1adf4b6
eleonorakuteva <eleonorakuteva@gmail.com>
2025-11-06 19:50:05 +0200

    Update waiter.py

--- restaurant_management/waiter.py
+++ restaurant_management/waiter.py
@@ -1,2 +1,14 @@
 class Waiter:
-    pass
+    def __init__(self, name:str, id_:int):
+        self.name = name
+        self.id = id_
+
+
+    @property
+    def id(self):
+        return self.__id
+
+    @id.setter
+    def id(self, value):
+        if value.isdigit() and len(value) == 10:
+            self.__id = value
