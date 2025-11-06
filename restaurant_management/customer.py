577be31aacc0b372a63e751fcdfb0836393fafc3
eleonorakuteva <eleonorakuteva@gmail.com>
2025-11-06 19:58:27 +0200

    Update customer.py

--- restaurant_management/customer.py
+++ restaurant_management/customer.py
@@ -1,2 +1,11 @@
 class Customer:
-    pass
+    def __init__(self, name:str) -> None:
+        self.name = name
+        self.order_list :list = []
+        self.total_bill = 0
+
+    def view_bill(self):
+        return self.total_bill
+
+    def order_item(self):
+        pass
