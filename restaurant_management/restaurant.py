579bfe81880b09aa10cefee82f3c4362919ba561
eleonorakuteva <eleonorakuteva@gmail.com>
2025-11-07 20:28:39 +0200

    Update restaurant.py

--- restaurant_management/restaurant.py
+++ restaurant_management/restaurant.py
@@ -3,11 +3,34 @@ from waiter import Waiter
 
 
 class Restaurant:
+    """
+    Purpose: Store the restaurant name, menu, waiters, and customers.
+
+    Key Methods:
+
+    add_waiter(waiter) → adds waiter to staff.
+
+    add_customer(customer) → registers a customer currently seated.
+
+    show_menu() → displays available dishes.
+
+    summary() → shows how many active orders and total earnings.
+    """
     def __init__(self, name:str) -> None:
         self.name = name
         self.__list_of_customers: list[Customer] = []
         self.__list_of_waiters: list[Waiter] = []
         self.total_earnings = 0
+
+    @property
+    def name(self):
+        return self.__name
+
+    @name.setter
+    def name(self, value: str):
+        if len(value) < 3 and value.strip().isalpha():
+            self.__name = value.title()
+        raise ValueError("Restaurant name must contain at least 3 letters.")
 
     def add_customer(self, customer:Customer):
         try:
