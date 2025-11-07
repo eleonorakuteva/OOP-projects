e320d74b6721a9b3cf7775b40fde3718ff32cb29
eleonorakuteva <eleonorakuteva@gmail.com>
2025-11-07 20:45:16 +0200

    Update payment_system.py

--- restaurant_management/payment_system.py
+++ restaurant_management/payment_system.py
@@ -1,6 +1,7 @@
 class PaymentSystem:
 
     PAYMENT_METHODS:list[str] = ["Cash", "Card"]
+    payment_history: list[dict[str, str | float]] = []
 
     def __init__(self):
         self.method: str = ""
@@ -8,3 +9,12 @@ class PaymentSystem:
         self.restaurant_name: str = ""
         self.amount_paid: float = 0.0
         self.tip: float = 0.0
+        PaymentSystem.payment_history.append(
+            {
+                "customer" : self.customer_name. title(),
+                "method" : self.method.lower(),
+                "amount" : self.amount_paid,
+                "tip" : self.tip,
+                "date" : "dd.mm.yyyy"
+             }
+        )
