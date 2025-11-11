0426cad26aa74ef8a05f33df92bcab06f01ed438
eleonorakuteva <eleonorakuteva@gmail.com>
2025-11-11 18:04:08 +0200

    Update summit_peak.py

--- climbers_and_peaks/project/peaks/summit_peak.py
+++ climbers_and_peaks/project/peaks/summit_peak.py
@@ -3,13 +3,12 @@ from project.peaks.base_peak import BasePeak
 
 class SummitPeak(BasePeak):
 
-    @property
-    def recommended_gear(self) -> list:
+    def get_recommended_gear(self) -> list[str]:
         return ["Climbing helmet", "Harness", "Climbing shoes", "Ropes"]
 
     def calculate_difficulty_level(self) -> str:
-        if 1_500 <= self.elevation <= 2_500:
-            return "Advanced"
-        elif self.elevation > 2_500:
+        if self.elevation > 2500:
             return "Extreme"
+        elif 1500 <= self.elevation <= 2500:
+            return "Advanced"
 
