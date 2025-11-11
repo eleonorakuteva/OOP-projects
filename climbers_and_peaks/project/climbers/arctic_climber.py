851f0cbcbb1cc9a48ebf54183fe077749ed5eabd
eleonorakuteva <eleonorakuteva@gmail.com>
2025-11-11 12:44:44 +0200

    Update arctic_climber.py

--- climbers_and_peaks/project/climbers/arctic_climber.py
+++ climbers_and_peaks/project/climbers/arctic_climber.py
@@ -1,4 +1,5 @@
 from project.climbers.base_climber import BaseClimber
+from project.peaks.base_peak import BasePeak
 
 
 class ArcticClimber(BaseClimber):
@@ -9,8 +10,16 @@ class ArcticClimber(BaseClimber):
     def __init__(self, name):
         super().__init__(name, ArcticClimber.INITIAL_STRENGTH)
 
-    def can_climb(self):
+    def can_climb(self) -> bool:
         return self.strength >= self.NEEDED_STRENGTH_FOR_CLIMBING
+
+    def climb(self, peak: BasePeak):
+        if peak.difficulty_level == "Extreme":
+            self.strength = (self.strength - 20) * 2
+        elif peak.difficulty_level == "Advanced":
+            self.strength = (self.strength - 20) * 1.5
+
+        self.conquered_peaks.append(peak.name)
 
 
 
