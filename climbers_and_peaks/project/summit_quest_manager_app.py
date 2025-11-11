d1c82189cefb064b09779c169c41d3fc24e82e69
eleonorakuteva <eleonorakuteva@gmail.com>
2025-11-11 13:49:29 +0200

    Update summit_quest_manager_app.py

---
+++ climbers_and_peaks/project/summit_quest_manager_app.py
@@ -0,0 +1,21 @@
+class SummitQuestManagerApp:
+    def __init__(self):
+        self.climbers: list = []
+        self.peaks: list = []
+
+
+    def register_climber(self, climber_type: str, climber_name: str):
+        pass
+
+    def peak_wish_list(self, peak_type: str, peak_name: str, peak_elevation: int):
+        pass
+
+    def check_gear(self, climber_name: str, peak_name: str, gear: List[str]):
+        pass
+
+    def perform_climbing(self, climber_name: str, peak_name: str):
+        pass
+
+    def get_statistics (self):
+        pass
+
