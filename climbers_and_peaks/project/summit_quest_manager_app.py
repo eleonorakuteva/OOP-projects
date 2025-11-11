19b5f55001de74aeda77d5bed668691c2f777d4f
eleonorakuteva <eleonorakuteva@gmail.com>
2025-11-11 17:05:05 +0200

    Update summit_quest_manager_app.py

--- climbers_and_peaks/project/summit_quest_manager_app.py
+++ climbers_and_peaks/project/summit_quest_manager_app.py
@@ -1,5 +1,3 @@
-from webbrowser import Error
-
 from project.climbers.arctic_climber import ArcticClimber
 from project.climbers.base_climber import BaseClimber
 from project.climbers.summit_climber import SummitClimber
@@ -116,42 +114,42 @@ class SummitQuestManagerApp:
             result.append(str(climber))
         return '\n'.join(result)
 
-# Create an instance of SummitQuestManagerApp
-climbing_app = SummitQuestManagerApp()
-
-# Register climbers
-print(climbing_app.register_climber("ArcticClimber", "Alice"))
-print(climbing_app.register_climber("SummitClimber", "Bob"))
-print(climbing_app.register_climber("ExtremeClimber", "Dave"))
-print(climbing_app.register_climber("ArcticClimber", "Charlie"))
-print(climbing_app.register_climber("ArcticClimber", "Alice"))
-print(climbing_app.register_climber("SummitClimber", "Eve"))
-print(climbing_app.register_climber("SummitClimber", "Frank"))
-
-# Add peaks to the wish list
-print(climbing_app.peak_wish_list("ArcticPeak", "MountEverest", 4000))
-print(climbing_app.peak_wish_list("SummitPeak", "K2", 3000))
-print(climbing_app.peak_wish_list("ArcticPeak", "Denali", 2500))
-print(climbing_app.peak_wish_list("UnchartedPeak", "MysteryMountain", 2000))
-
-# Prepare climbers for climbing
-print(climbing_app.check_gear("Alice", "MountEverest", ["Ice axe", "Crampons", "Insulated clothing", "Helmet"]))
-print(climbing_app.check_gear("Bob", "K2", ["Climbing helmet", "Harness", "Climbing shoes", "Ropes"]))
-print(climbing_app.check_gear("Charlie", "Denali", ["Ice axe", "Crampons"]))
-
-# Perform climbing
-print(climbing_app.perform_climbing("Alice", "MountEverest"))
-print(climbing_app.perform_climbing("Bob", "K2"))
-print(climbing_app.perform_climbing("Kelly", "Denali"))
-print(climbing_app.perform_climbing("Alice", "K2"))
-print(climbing_app.perform_climbing("Alice", "MysteryMountain"))
-print(climbing_app.perform_climbing("Eve", "MountEverest"))
-print(climbing_app.perform_climbing("Charlie", "MountEverest"))
-print(climbing_app.perform_climbing("Frank", "K2"))
-print(climbing_app.perform_climbing("Frank", "Denali"))
-print(climbing_app.perform_climbing("Frank", "MountEverest"))
-
-# Get statistics
-print(climbing_app.get_statistics())
+# # Create an instance of SummitQuestManagerApp
+# climbing_app = SummitQuestManagerApp()
+#
+# # Register climbers
+# print(climbing_app.register_climber("ArcticClimber", "Alice"))
+# print(climbing_app.register_climber("SummitClimber", "Bob"))
+# print(climbing_app.register_climber("ExtremeClimber", "Dave"))
+# print(climbing_app.register_climber("ArcticClimber", "Charlie"))
+# print(climbing_app.register_climber("ArcticClimber", "Alice"))
+# print(climbing_app.register_climber("SummitClimber", "Eve"))
+# print(climbing_app.register_climber("SummitClimber", "Frank"))
+#
+# # Add peaks to the wish list
+# print(climbing_app.peak_wish_list("ArcticPeak", "MountEverest", 4000))
+# print(climbing_app.peak_wish_list("SummitPeak", "K2", 3000))
+# print(climbing_app.peak_wish_list("ArcticPeak", "Denali", 2500))
+# print(climbing_app.peak_wish_list("UnchartedPeak", "MysteryMountain", 2000))
+#
+# # Prepare climbers for climbing
+# print(climbing_app.check_gear("Alice", "MountEverest", ["Ice axe", "Crampons", "Insulated clothing", "Helmet"]))
+# print(climbing_app.check_gear("Bob", "K2", ["Climbing helmet", "Harness", "Climbing shoes", "Ropes"]))
+# print(climbing_app.check_gear("Charlie", "Denali", ["Ice axe", "Crampons"]))
+#
+# # Perform climbing
+# print(climbing_app.perform_climbing("Alice", "MountEverest"))
+# print(climbing_app.perform_climbing("Bob", "K2"))
+# print(climbing_app.perform_climbing("Kelly", "Denali"))
+# print(climbing_app.perform_climbing("Alice", "K2"))
+# print(climbing_app.perform_climbing("Alice", "MysteryMountain"))
+# print(climbing_app.perform_climbing("Eve", "MountEverest"))
+# print(climbing_app.perform_climbing("Charlie", "MountEverest"))
+# print(climbing_app.perform_climbing("Frank", "K2"))
+# print(climbing_app.perform_climbing("Frank", "Denali"))
+# print(climbing_app.perform_climbing("Frank", "MountEverest"))
+#
+# # Get statistics
+# print(climbing_app.get_statistics())
 
 
