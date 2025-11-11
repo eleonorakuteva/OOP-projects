fc6ad0c0a562c2ff8f0f06a644406a92e74ddd4e
eleonorakuteva <eleonorakuteva@gmail.com>
2025-11-11 17:10:00 +0200

    Update arctic_climber.py

--- climbers_and_peaks/project/climbers/arctic_climber.py
+++ climbers_and_peaks/project/climbers/arctic_climber.py
@@ -5,7 +5,7 @@ from project.peaks.base_peak import BasePeak
 class ArcticClimber(BaseClimber):
 
     INITIAL_STRENGTH: float = 200
-    NEEDED_STRENGTH_FOR_CLIMBING: int = 100
+    NEEDED_STRENGTH_FOR_CLIMBING: float = 100
 
     def __init__(self, name):
         super().__init__(name, ArcticClimber.INITIAL_STRENGTH)
