99cfdb51b8f8cce25c22989d73895306d285caaf
eleonorakuteva <eleonorakuteva@gmail.com>
2025-11-11 11:50:34 +0200

    Update summit_peak.py

--- climbers_and_peaks/project/peaks/summit_peak.py
+++ climbers_and_peaks/project/peaks/summit_peak.py
@@ -8,7 +8,7 @@ class SummitPeak(BasePeak):
         return ["Climbing helmet", "Harness", "Climbing shoes", "Ropes"]
 
     @property
-    def difficulty_level(self) -> dict:
+    def difficulty_levels(self) -> dict:
         levels = {"Advanced": range(1500, 2501), "Extreme": range(2_501, BasePeak.EVEREST_HEIGHT + 1)}
         return levels
 
