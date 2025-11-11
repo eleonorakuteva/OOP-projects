c40e202fbd788244ae0251bb5b46d9471297ae66
eleonorakuteva <eleonorakuteva@gmail.com>
2025-11-11 12:44:57 +0200

    Update summit_climber.py

--- climbers_and_peaks/project/climbers/summit_climber.py
+++ climbers_and_peaks/project/climbers/summit_climber.py
@@ -1,4 +1,5 @@
 from project.climbers.base_climber import BaseClimber
+from project.peaks.base_peak import BasePeak
 
 
 class SummitClimber(BaseClimber):
@@ -9,5 +10,15 @@ class SummitClimber(BaseClimber):
     def __init__(self, name):
         super().__init__(name, SummitClimber.INITIAL_STRENGTH)
 
-    def can_climb(self):
+    def can_climb(self) -> bool:
         return self.strength >= self.NEEDED_STRENGTH_FOR_CLIMBING
+
+
+    def climb(self, peak: BasePeak):
+        if peak.difficulty_level == "Extreme":
+            self.strength = (self.strength - 30) * 2.5
+
+        elif peak.difficulty_level == "Advanced":
+            self.strength = (self.strength - 30) * 1.3
+
+        self.conquered_peaks.append(peak.name)
