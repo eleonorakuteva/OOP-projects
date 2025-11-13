52a0ef9cdd6cf47b29a1ab6d086502cf2d7d5b0b
eleonorakuteva <eleonorakuteva@gmail.com>
2025-11-13 14:40:24 +0200

    Update nautical_catch_challenge_app.py

---
+++ fish_and_divers/project/nautical_catch_challenge_app.py
@@ -0,0 +1,26 @@
+from project.divers.base_diver import BaseDiver
+from project.fish.base_fish import BaseFish
+
+class NauticalCatchChallengeApp:
+
+    def __init__(self):
+        self.divers: list[BaseDiver] = []
+        self.fish_list: list[BaseFish] = []
+
+    def dive_into_competition(self, diver_type: str, diver_name: str):
+        pass
+
+    def swim_into_competition(self, fish_type: str, fish_name: str, points: float):
+        pass
+
+    def chase_fish(self, diver_name: str, fish_name: str, is_lucky: bool):
+        pass
+
+    def health_recovery(self):
+        pass
+
+    def diver_catch_report(self, diver_name: str):
+        pass
+
+    def competition_statistics(self):
+        pass
