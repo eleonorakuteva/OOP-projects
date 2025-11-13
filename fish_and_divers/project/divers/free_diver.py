1b7dedff52aadd42956ed934348796c77ea83d3d
eleonorakuteva <eleonorakuteva@gmail.com>
2025-11-13 18:40:55 +0200

    Update free_diver.py

--- fish_and_divers/project/divers/free_diver.py
+++ fish_and_divers/project/divers/free_diver.py
@@ -2,6 +2,7 @@ from project.divers.base_diver import BaseDiver
 
 
 class FreeDiver(BaseDiver):
+
     INITIAL_OXYGEN_LEVEL = 120
     DECREASED_PERCENTAGE_PER_MISS: float = 0.6
 
@@ -9,8 +10,8 @@ class FreeDiver(BaseDiver):
         super().__init__(name, self.INITIAL_OXYGEN_LEVEL)
 
     def miss(self, time_to_catch: int) -> None:
-        decreased = int(round(time_to_catch * self.DECREASED_PERCENTAGE_PER_MISS))
-        self.oxygen_level = max(0, (self.oxygen_level - decreased))
+        reduce_amount = int(round(time_to_catch * self.DECREASED_PERCENTAGE_PER_MISS))
+        self.oxygen_level = max(0, (self.oxygen_level - reduce_amount))
 
     def renew_oxy(self) -> None:
         self.oxygen_level = self.INITIAL_OXYGEN_LEVEL
