24778d41ce11457d0cfcc7b3947fe02531501334
eleonorakuteva <eleonorakuteva@gmail.com>
2025-11-13 17:52:41 +0200

    Update predatory_fish.py

--- fish_and_divers/project/fish/predatory_fish.py
+++ fish_and_divers/project/fish/predatory_fish.py
@@ -1,13 +1,14 @@
 from project.fish.base_fish import BaseFish
 
 
-
 class PredatoryFish(BaseFish):
+
     TIME_TO_CATCH = 90
+
     def __init__(self, name:str, points: float):
         super().__init__(name= name, points=points, time_to_catch=self.TIME_TO_CATCH)
 
     def fish_details(self) -> str:
         return (f"{type(self).__name__}: {self.name} "
                 f"[Points: {self.points}, "
-                f"Time to Catch: {self.TIME_TO_CATCH} seconds]")
+                f"Time to Catch: {self.time_to_catch} seconds]")
