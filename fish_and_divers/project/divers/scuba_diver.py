49be98e174d598e252bc0d99e5a274628546afc6
eleonorakuteva <eleonorakuteva@gmail.com>
2025-11-13 18:40:50 +0200

    Update scuba_diver.py

--- fish_and_divers/project/divers/scuba_diver.py
+++ fish_and_divers/project/divers/scuba_diver.py
@@ -2,6 +2,7 @@ from project.divers.base_diver import BaseDiver
 
 
 class ScubaDiver(BaseDiver):
+
     INITIAL_OXYGEN_LEVEL = 540
     DECREASED_PERCENTAGE_PER_MISS = 0.3
 
@@ -11,7 +12,6 @@ class ScubaDiver(BaseDiver):
     def miss(self, time_to_catch: int) -> None:
         decreased = int(round(time_to_catch * self.DECREASED_PERCENTAGE_PER_MISS))
         self.oxygen_level = max(0, (self.oxygen_level - decreased))
-
 
     def renew_oxy(self) -> None:
         self.oxygen_level = self.INITIAL_OXYGEN_LEVEL
