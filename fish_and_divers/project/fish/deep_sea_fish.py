b207850bc32153f7ffb68693afce395dd6b8abfb
eleonorakuteva <eleonorakuteva@gmail.com>
2025-11-13 14:10:49 +0200

    Update deep_sea_fish.py

---
+++ fish_and_divers/project/fish/deep_sea_fish.py
@@ -0,0 +1,13 @@
+from project.fish.base_fish import BaseFish
+
+
+class DeepSeaFish(BaseFish):
+    TIME_TO_CATCH = 180
+
+    def __init__(self, name: str, points: float):
+        super().__init__(name=name, points=points, time_to_catch=self.TIME_TO_CATCH)
+
+    def fish_details(self) -> str:
+        return (f"{type(self).__name__}: {self.name} "
+                f"[Points: {self.points}, "
+                f"Time to Catch: {self.TIME_TO_CATCH} seconds]")
