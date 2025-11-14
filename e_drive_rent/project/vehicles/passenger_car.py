6d3cce706893bb2e10a78189b3805d444292195e
eleonorakuteva <eleonorakuteva@gmail.com>
2025-11-14 19:21:23 +0200

    Update passenger_car.py

--- e_drive_rent/project/vehicles/passenger_car.py
+++ e_drive_rent/project/vehicles/passenger_car.py
@@ -1,4 +1,15 @@
-from project.
+from project.vehicles.base_vehicle import BaseVehicle
 
-class PassengerCar(BaseCar):
-    pass
+
+class PassengerCar(BaseVehicle):
+
+    MAX_MILEAGE = 450.00
+
+    def __init__(self, brand: str, model: str, license_plate_number: str):
+        super().__init__(brand,model, license_plate_number, max_mileage= self.MAX_MILEAGE)
+
+
+    def drive(self, mileage: float):
+        percentage = (mileage / self.MAX_MILEAGE) * 100
+        round_percentage = round(percentage)
+        self.battery_level -= round_percentage
