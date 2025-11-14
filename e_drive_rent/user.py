d8f8a229b18dacba5f1a28765d2425d4cf3b9efe
eleonorakuteva <eleonorakuteva@gmail.com>
2025-11-14 18:32:09 +0200

    Update user.py

--- e_drive_rent/user.py
+++ e_drive_rent/user.py
@@ -1,4 +1,8 @@
 class User:
+
+    MAX_RATING: float = 10.0
+    MIN_RATING: float = 0.0
+
     def __init__(self, first_name: str, last_name: str, driving_license_number: str):
         self.first_name = first_name
         self.last_name = last_name
@@ -14,6 +18,10 @@ class User:
     def rating(self, value: float):
         if value < 0:
             raise ValueError("Users rating cannot be negative!")
+        # ?
+        elif value == 0:
+            self.is_blocked = True
+        # ?
         self.__rating = value
 
     @property
@@ -45,3 +53,16 @@ class User:
         if not value.strip():
             raise ValueError("Driving license number is required!")
         self.__driving_license_number = value
+
+    def increase_rating(self):
+        increased_amount = self.__rating + 0.5
+        self.__rating = min(self.MAX_RATING, increased_amount)
+
+    def decrease_rating(self):
+        decreased_rating = self.__rating - 2.0
+        self.__rating = max(0, decreased_rating)
+
+    def __str__(self):
+        return (f"{self.first_name} {self.last_name} "
+                f"Driving license: {self.driving_license_number} "
+                f"Rating: {self.rating}")
