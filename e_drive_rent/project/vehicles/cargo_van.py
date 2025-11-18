b5d05bb3f546ef43a1c9745fefa387b4be2a120f
eleonorakuteva <eleonorakuteva@gmail.com>
2025-11-18 19:24:42 +0200

    Update cargo_van.py

--- e_drive_rent/project/vehicles/cargo_van.py
+++ e_drive_rent/project/vehicles/cargo_van.py
@@ -1,4 +1,5 @@
 from project.vehicles.base_vehicle import BaseVehicle
+from math import floor
 
 
 class CargoVan(BaseVehicle):
@@ -6,11 +7,11 @@ class CargoVan(BaseVehicle):
     MAX_MILEAGE = 180
 
     def __init__(self, brand: str, model: str, license_plate_number: str):
-        super().__init__(brand, model, license_plate_number, max_mileage= self.MAX_MILEAGE)
+        super().__init__(brand, model, license_plate_number, max_mileage= CargoVan.MAX_MILEAGE)
 
 
     def drive(self, mileage: float):
         # when driving CargoVan, you should reduce an additional 5%, because of the load.
-        percentage = (mileage / self.MAX_MILEAGE) * 100
-        round_percentage = round(percentage + 5)
+        percentage = ((mileage / CargoVan.MAX_MILEAGE) * 100) + 5
+        round_percentage = floor(percentage + 0.5)
         self.battery_level -= round_percentage
