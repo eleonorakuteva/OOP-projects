f8f3e0c35085d736edc8ecb2ca8a9eca795677d8
eleonorakuteva <eleonorakuteva@gmail.com>
2025-11-18 14:02:05 +0200

    Update managing_app.py

--- e_drive_rent/project/managing_app.py
+++ e_drive_rent/project/managing_app.py
@@ -10,7 +10,14 @@ class ManagingApp:
         self.routes: list[Route] = []
 
     def register_user(self, first_name: str, last_name: str, driving_license_number: str):
-        pass
+        try:
+            next(u for u in self.users if u.driving_license_number == driving_license_number)
+            return f"{driving_license_number} has already been registered to our platform."
+        except StopIteration:
+            new_user = User(first_name, last_name, driving_license_number)
+            self.users.append(new_user)
+            return f"{first_name} {last_name} was successfully registered under DLN-{driving_license_number}"
+
 
     def upload_vehicle(self, vehicle_type: str, brand: str, model: str, license_plate_number: str):
         pass
@@ -26,3 +33,28 @@ class ManagingApp:
 
     def users_report(self):
         pass
+
+app = ManagingApp()
+print(app.register_user( 'Tisha', 'Reenie', '7246506' ))
+print(app.register_user( 'Bernard', 'Remy', 'CDYHVSR68661'))
+print(app.register_user( 'Mack', 'Cindi', '7246506'))
+print(app.upload_vehicle('PassengerCar', 'Chevrolet', 'Volt', 'CWP8032'))
+print(app.upload_vehicle( 'PassengerCar', 'Volkswagen', 'e-Up!', 'COUN199728'))
+print(app.upload_vehicle('PassengerCar', 'Mercedes-Benz', 'EQS', '5UNM315'))
+print(app.upload_vehicle('CargoVan', 'Ford', 'e-Transit', '726QOA'))
+print(app.upload_vehicle('CargoVan', 'BrightDrop', 'Zevo400', 'SC39690'))
+print(app.upload_vehicle('EcoTruck', 'Mercedes-Benz', 'eActros', 'SC39690'))
+print(app.upload_vehicle('PassengerCar', 'Tesla', 'CyberTruck', '726QOA'))
+print(app.allow_route('SOF', 'PLD', 144))
+print(app.allow_route('BUR', 'VAR', 87))
+print(app.allow_route('BUR', 'VAR', 87))
+print(app.allow_route('SOF', 'PLD', 184))
+print(app.allow_route('BUR', 'VAR', 86.999))
+print(app.make_trip('CDYHVSR68661', '5UNM315', 3, False))
+print(app.make_trip('7246506', 'CWP8032', 1, True))
+print(app.make_trip('7246506', 'COUN199728', 1, False))
+print(app.make_trip('CDYHVSR68661', 'CWP8032', 3, False))
+print(app.make_trip('CDYHVSR68661', '5UNM315', 2, False))
+print(app.repair_vehicles(2))
+print(app.repair_vehicles(20))
+print(app.users_report())
