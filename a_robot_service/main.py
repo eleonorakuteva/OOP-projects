5b70fea77edce33b5e41a838d84a06e9ac19dbb5
eleonorakuteva <eleonorakuteva@gmail.com>
2025-11-19 19:33:19 +0200

    Update main.py

--- a_robot_service/main.py
+++ a_robot_service/main.py
@@ -4,8 +4,11 @@ from project.robots_managing_app import RobotsManagingApp
 main_app = RobotsManagingApp()
 print(main_app.add_service('SecondaryService', 'ServiceRobotsWorld'))
 print(main_app.add_service('MainService', 'ServiceTechnicalsWorld'))
+# print(main_app.add_service('Mainservice', 'ServiceTechnicalsWorld'))
+
 print(main_app.add_robot('FemaleRobot', 'Scrap', 'HouseholdRobots', 321.26))
 print(main_app.add_robot('FemaleRobot', 'Sparkle', 'FunnyRobots', 211.11))
+# print(main_app.add_robot('emaleRobot', 'Sparkle', 'FunnyRobots', 211.11))
 
 print(main_app.add_robot_to_service('Scrap', 'ServiceRobotsWorld'))
 print(main_app.add_robot_to_service('Sparkle', 'ServiceRobotsWorld'))
