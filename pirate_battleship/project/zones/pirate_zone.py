901f548f2f9d0b9ce3ad8d851aae0c4c5cccd1a9
eleonorakuteva <eleonorakuteva@gmail.com>
2025-11-21 14:18:11 +0200

    Update pirate_zone.py

--- pirate_battleship/project/zones/pirate_zone.py
+++ pirate_battleship/project/zones/pirate_zone.py
@@ -8,13 +8,13 @@ class PirateZone(BaseZone):
         super().__init__(code=code, volume=self.INITIAL_VOLUME)
 
     def zone_info(self):
-        result = ["@Royal Zone Statistics@",
+        result = ["@Pirate Zone Statistics@",
                   f"Code: {self.code}; Volume: {self.volume}"]
 
-        pirateships_count = sum(s for s in self.ships if type(self).__name__ == "PirateBattleship")
+        royalships_count = sum(s for s in self.ships if type(self).__name__ == "RoyalBattleship")
 
         result.append(
-            f"Battleships currently in the Pirate Zone: {len(self.ships)}, {pirateships_count} out of them are Pirate Battleships.")
+            f"Battleships currently in the Pirate Zone: {len(self.ships)}, {royalships_count} out of them are Pirate Battleships.")
 
         if self.ships:
             result.append(f"#{', '.join(self.get_ships())}#")
