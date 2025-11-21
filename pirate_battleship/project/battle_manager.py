71c8fa7219bcd68d4ddc2cb339360e671876ac3e
eleonorakuteva <eleonorakuteva@gmail.com>
2025-11-21 13:27:35 +0200

    Update battle_manager.py

--- pirate_battleship/project/battle_manager.py
+++ pirate_battleship/project/battle_manager.py
@@ -1,2 +1,17 @@
+from project.battleships.base_battleship import BaseBattleship
+from project.zones.base_zone import BaseZone
+from project.zones.royal_zone import RoyalZone
+from project.zones.pirate_zone import PirateZone
+
+
 class BattleManager:
-    pass
+
+    VALID_ZONES = {"RoyalZone": RoyalZone, "PirateZone": PirateZone}
+
+
+    def __init__(self):
+        self.zones: list[BaseZone] = []
+        self.ships: list[BaseBattleship] = []
+
+    def add_zone(self, zone_type: str, zone_code: str):
+        pass
