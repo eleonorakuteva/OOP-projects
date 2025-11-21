041342e33c271e8d373323a0d940d625449fbb2b
eleonorakuteva <eleonorakuteva@gmail.com>
2025-11-21 13:23:59 +0200

    Update pirate_zone.py

---
+++ pirate_battleship/project/zones/pirate_zone.py
@@ -0,0 +1,21 @@
+from project.zones.base_zone import BaseZone
+
+
+class RoyalZone(BaseZone):
+    INITIAL_VOLUME = 8
+
+    def __init__(self, code: str):
+        super().__init__(code=code, volume=self.INITIAL_VOLUME)
+
+    def zone_info(self):
+        result = ["@Royal Zone Statistics@",
+                  f"Code: {self.code}; Volume: {self.volume}"]
+
+        pirateships_count = sum(s for s in self.ships if type(self).__name__ == "PirateBattleship")
+
+        result.append(
+            f"Battleships currently in the Pirate Zone: {len(self.ships)}, {pirateships_count} out of them are Pirate Battleships.")
+
+        if self.ships:
+            result.append(f"#{', '.join(self.get_ships())}#")
+
