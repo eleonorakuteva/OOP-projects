9c9fbfaf8f00d2e648a6d8dc2bba7e5b7dd57dd7
eleonorakuteva <eleonorakuteva@gmail.com>
2025-11-23 18:21:26 +0200

    Update desktop_computer.py

--- computer_store/project/computer_types/desktop_computer.py
+++ computer_store/project/computer_types/desktop_computer.py
@@ -3,21 +3,20 @@ from project.computer_types.computer import Computer
 
 class DesktopComputer(Computer):
 
-    PROCESSORS = {"AMD Ryzen 7 5700G": 500,
-                  "Intel Core i5-12600K": 600,
-                  "Apple M1 Max": 1800}
-
-    MAX_RAM = 128 #GB
-
     def __init__(self, manufacturer: str, model: str):
         super().__init__(manufacturer= manufacturer, model=model)
 
-    def configure_computer(self, processor: str, ram: int):
-        if processor not in self.PROCESSORS:
-            raise ValueError(f"{processor} is not compatible with desktop computer {self.manufacturer} {self.model}!")
 
-        if self.MAX_RAM:
-            pass
+    @property
+    def max_ram(self) -> int:
+        return 128 #GB
 
-        else:
-            pass
+    @property
+    def valid_processors(self) -> dict[str: int]:
+        return {"AMD Ryzen 7 5700G": 500,
+                "Intel Core i5-12600K": 600,
+                "Apple M1 Max": 1800}
+
+    @property
+    def type_computer(self):
+        return "desktop computer"
