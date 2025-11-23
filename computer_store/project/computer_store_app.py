d871534f82c7c7aedd730987c22b792f99be1ef6
eleonorakuteva <eleonorakuteva@gmail.com>
2025-11-23 19:35:19 +0200

    Update computer_store_app.py

--- computer_store/project/computer_store_app.py
+++ computer_store/project/computer_store_app.py
@@ -15,19 +15,22 @@ class ComputerStoreApp:
 
     def build_computer(self, type_computer: str, manufacturer: str, model: str, processor: str, ram: int):
         try:
-            new_computer: Computer = self.VALID_TYPES_COMPUTERS[type_computer](manufacturer, model)
-            result = new_computer.configure_computer(processor, ram)
-            self.warehouse.append(new_computer)
-            return result
+            computer = self.VALID_TYPES_COMPUTERS[type_computer](manufacturer, model)
         except KeyError:
-            return ValueError(f"{type_computer} is not a valid type computer!")
+            raise ValueError(f"{type_computer} is not a valid type computer!")
+
+        configuration: str = computer.configure_computer(processor, ram)
+
+        self.warehouse.append(computer)
+
+        return configuration
 
     def sell_computer(self, client_budget: int, wanted_processor: str, wanted_ram: int):
 
         has_wanted_computer = next((c for c in self.warehouse
-                                                 if c.price <= client_budget
-                                                 and c.processor == wanted_processor
-                                                 and c.ram >= wanted_ram), None)
+                                    if c.price <= client_budget
+                                    and c.processor == wanted_processor
+                                    and wanted_ram >= c.ram), None)
 
         if has_wanted_computer is None:
             raise Exception("Sorry, we don't have a computer for you.")
@@ -35,4 +38,5 @@ class ComputerStoreApp:
         price_diff = client_budget - has_wanted_computer.price
         self.profits += price_diff
         self.warehouse.remove(has_wanted_computer)
+
         return f"{has_wanted_computer} sold for {client_budget}$."
