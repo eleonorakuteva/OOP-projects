bbdbeaa88031bb20d916249f779cd1c6b953f3f9
eleonorakuteva <eleonorakuteva@gmail.com>
2025-11-23 19:09:19 +0200

    Update computer_store_app.py

--- computer_store/project/computer_store_app.py
+++ computer_store/project/computer_store_app.py
@@ -14,7 +14,25 @@ class ComputerStoreApp:
         self.profits: int = 0
 
     def build_computer(self, type_computer: str, manufacturer: str, model: str, processor: str, ram: int):
-        pass
+        try:
+            new_computer: Computer = self.VALID_TYPES_COMPUTERS[type_computer](manufacturer, model)
+            result = new_computer.configure_computer(processor, ram)
+            self.warehouse.append(new_computer)
+            return result
+        except KeyError:
+            return ValueError(f"{type_computer} is not a valid type computer!")
 
     def sell_computer(self, client_budget: int, wanted_processor: str, wanted_ram: int):
-        pass
+
+        has_wanted_computer = next((c for c in self.warehouse
+                                                 if c.price <= client_budget
+                                                 and c.processor == wanted_processor
+                                                 and c.ram >= wanted_ram), None)
+
+        if has_wanted_computer is None:
+            raise Exception("Sorry, we don't have a computer for you.")
+
+        price_diff = client_budget - has_wanted_computer.price
+        self.profits += price_diff
+        self.warehouse.remove(has_wanted_computer)
+        return f"{has_wanted_computer} sold for {client_budget}$."
