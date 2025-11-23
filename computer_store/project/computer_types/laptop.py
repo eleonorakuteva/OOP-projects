61aa7af85e3d4e04f209d70bc44f9e201f00ca7a
eleonorakuteva <eleonorakuteva@gmail.com>
2025-11-23 18:21:37 +0200

    Update laptop.py

--- computer_store/project/computer_types/laptop.py
+++ computer_store/project/computer_types/laptop.py
@@ -2,21 +2,20 @@ from project.computer_types.computer import Computer
 
 
 class Laptop(Computer):
-    PROCESSORS = {"AMD Ryzen 9 5950X": 900,
-                  "Intel Core i9-11900H": 1_050,
-                  "Apple M1 Pro": 1_200}
-
-    MAX_RAM = 64  # GB
 
     def __init__(self, manufacturer: str, model: str):
         super().__init__(manufacturer=manufacturer, model=model)
 
-    def configure_computer(self, processor: str, ram: int):
-        if processor not in self.PROCESSORS:
-            raise ValueError(f"{processor} is not compatible with laptop {self.manufacturer} {self.model}!")
+    @property
+    def max_ram(self):
+        return 64
 
-        if self.MAX_RAM:
-            pass
+    @property
+    def valid_processors(self) -> dict[str: int]:
+        return {"AMD Ryzen 9 5950X": 900,
+                "Intel Core i9-11900H": 1_050,
+                "Apple M1 Pro": 1_200}
 
-        else:
-            pass
+    @property
+    def type_computer(self):
+        return "laptop"
