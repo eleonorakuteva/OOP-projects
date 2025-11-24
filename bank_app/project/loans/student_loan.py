5f899651688a1209ec18aed7f8db166b46f88ed2
eleonorakuteva <eleonorakuteva@gmail.com>
2025-11-24 12:31:44 +0200

    Update student_loan.py

---
+++ bank_app/project/loans/student_loan.py
@@ -0,0 +1,15 @@
+from project.loans.base_loan import BaseLoan
+
+
+class StudentLoan(BaseLoan):
+
+    INTEREST_RATE = 1.5
+    INCREASE_INTEREST_RATE = 0.2
+    AMOUNT = 2_000.00
+
+    def __init__(self):
+        super().__init__(self.interest_rate, self.AMOUNT)
+
+    def increase_interest_rate(self):
+        self.interest_rate *= self.INCREASE_INTEREST_RATE
+
