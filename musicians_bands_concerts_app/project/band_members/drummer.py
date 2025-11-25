8012c98e08cb97b5b62477c7513397ff714fa9d4
eleonorakuteva <eleonorakuteva@gmail.com>
2025-11-25 14:37:01 +0200

    Update drummer.py

---
+++ musicians_bands_concerts_app/project/band_members/drummer.py
@@ -0,0 +1,13 @@
+from project.band_members.musician import Musician
+
+
+class Drummer(Musician):
+    def __init__(self, name: str, age: int):
+        super().__init__(name, age)
+
+    @property
+    def available_skills_for_type_of_musician(self) -> list[str]:
+        return ["play the drums with drumsticks",
+                "play the drums with drum brushes",
+                "read sheet music"]
+
