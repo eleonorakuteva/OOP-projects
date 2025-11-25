1e400eb0e5a8dd0a8000ac032de7eb2fc991204b
eleonorakuteva <eleonorakuteva@gmail.com>
2025-11-25 14:37:17 +0200

    Update singer.py

---
+++ musicians_bands_concerts_app/project/band_members/singer.py
@@ -0,0 +1,12 @@
+from project.band_members.musician import Musician
+
+
+class Singer(Musician):
+    def __init__(self, name: str, age: int):
+        super().__init__(name, age)
+
+    @property
+    def available_skills_for_type_of_musician(self) -> list[str]:
+        return ["sing high pitch notes", "sing low pitch notes"]
+    
+    
