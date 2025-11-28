de2aea2c590641a1897b034c216210b32cb35dbb
eleonorakuteva <eleonorakuteva@gmail.com>
2025-11-28 09:26:45 +0200

    Update main_dish.py

--- food_orders_app/project/meals/main_dish.py
+++ food_orders_app/project/meals/main_dish.py
@@ -1,34 +1,13 @@
-from abc import ABC, abstractmethod
+from project.meals.meal import Meal
 
 
-class Meal(ABC):
+class MainDish(Meal):
 
-    def __init__(self, name: str, price: float, quantity: int):
-        self.name = name
-        self.price = price
-        self.quantity = quantity
+    INITIAL_QUANTITY = 50
 
-    @property
-    def name(self):
-        return self.__name
+    def __init__(self, name, price, quantity = INITIAL_QUANTITY):
+        super().__init__(name, price, quantity)
 
-    @name.setter
-    def name(self, value: str):
-        if not value.strip():
-            raise ValueError("Name cannot be an empty string!")
-        self.__name = value
-
-    @property
-    def price(self):
-        return self.__price
-
-    @price.setter
-    def price(self, value):
-        if value <= 0.0:
-            raise ValueError("Invalid price!")
-        self.__price = value
-
-    @abstractmethod
     def details(self) -> str:
-        pass
+        return f"Main Dish {self.name}: {self.price:.2f}lv/piece"
 
