ba52278261a32f06f53ca1b50d86e79f4497a8af
eleonorakuteva <eleonorakuteva@gmail.com>
2025-12-01 11:51:25 +0200

    Update fantasy.py

---
+++ movie_app/project/movie_specification/fantasy.py
@@ -0,0 +1,28 @@
+from project.movie_specifications.movie import Movie
+
+
+class Fantasy(Movie):
+
+    MINIMUM_AGE_RESTRICTIONS = 6
+
+    def __init__(self, title: str, year: int, owner: object, age_restriction: int = self.MINIMUM_AGE_RESTRICTIONS):
+        super().__init__(title, year, owner, age_restriction = age_restriction)
+
+    @property
+    def age_restriction(self):
+        return self.__age_restriction
+
+    @age_restriction.setter
+    def age_restriction(self, value: int):
+        if value < self.MINIMUM_AGE_RESTRICTIONS:
+            raise ValueError("Fantasy movies must be restricted for audience under 6 years!")
+        self.__age_restriction = value
+
+    def details(self):
+        return (f"Fantasy - Title:{self.title}, "
+                f"Year:{self.year}, "
+                f"Age restriction:{self.age_restriction}, "
+                f"Likes:{self.likes}, "
+                f"Owned by:{self.owner.username}")
+
+    
