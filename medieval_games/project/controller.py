3d6e4b96614d2f4f1516ede57b1d0c294ad5b4cd
eleonorakuteva <eleonorakuteva@gmail.com>
2025-12-01 19:45:23 +0200

    Update controller.py

---
+++ medieval_games/project/controller.py
@@ -0,0 +1,28 @@
+from project.player import Player
+from project.supply.supply import Supply
+
+
+class Controller:
+
+    def __init__(self):
+        self.players: list[Player] = []
+        self.supplies: list[Supply] = []
+
+    def add_player(self, *players: Player):
+        pass
+
+    def add_supply(self, supplies: Supply):
+        pass
+
+    def sustain(self, player_name: str, sustenance_type: str):
+        pass
+
+    def duel(self, first_player_name: str, second_player_name: str):
+        pass
+
+    def next_day(self):
+        pass
+
+    def __str__(self):
+        pass
+    
