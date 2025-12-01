442854b504d45e95361bfcca04e03550a5a41000
eleonorakuteva <eleonorakuteva@gmail.com>
2025-12-01 19:49:47 +0200

    Update drink.py

--- medieval_games/project/supply/drink.py
+++ medieval_games/project/supply/drink.py
@@ -4,8 +4,10 @@ from project.supply.supply import Supply
 class Drink(Supply):
     INITIAL_UNITS_OF_ENERGY = 15
 
-    def __init__(self, name):
-        super().__init__(name, self.INITIAL_UNITS_OF_ENERGY)
+    def __init__(self, name, energy = None):
+        if energy is None:
+            energy = self.INITIAL_UNITS_OF_ENERGY
+        super().__init__(name, energy)
 
     def details(self):
         return f"{type(self).__name__}: {self.name}, {self.energy}"
