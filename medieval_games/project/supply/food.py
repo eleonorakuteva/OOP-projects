3d616b29d3e9dfbcdfbbbbb0d61687f8d87c731c
eleonorakuteva <eleonorakuteva@gmail.com>
2025-12-01 19:18:23 +0200

    Update food.py

---
+++ medieval_games/project/supply/food.py
@@ -0,0 +1,13 @@
+from project.supply.supply import Supply
+
+
+class Food(Supply):
+
+    INITIAL_UNITS_OF_ENERGY = 25
+
+    def __init__(self, name):
+        super().__init__(name, self.INITIAL_UNITS_OF_ENERGY)
+
+    def details(self):
+        return f"{type(self).__name__}: {self.name}, {self.energy}"
+
