02b07dc3f089a6aae7f7395dfac9d72b420e3dc7
eleonorakuteva <eleonorakuteva@gmail.com>
2025-12-02 13:09:19 +0200

    Update controller.py

--- car_races/project/controller.py
+++ car_races/project/controller.py
@@ -46,15 +46,45 @@ class Controller:
         self.races.append(new_race)
         return f"Race {race_name} is created."
 
-    def _driver_found_by_name(self, driver_name)-> bool:
+
+    def add_car_to_driver(self, driver_name: str, car_type: str):
+        curr_driver = self._driver_found_by_name(driver_name)
+        if not curr_driver:
+            raise Exception(f"Driver {driver_name} could not be found!")
+
+        curr_car = self._last_available_car_found_by_type(car_type)
+        if not curr_car:
+            raise Exception(f"Car {car_type} could not be found!")
+
+        # if driver doesn't have a car
+        if curr_driver.car is None:
+            return f"Driver {driver_name} chose the car {curr_car.model}."
+
+        # if driver has a car
+        old_model = curr_driver.car.model
+        # change the old one with the curr one
+        curr_driver.car = curr_car
+        new_model = curr_driver.car.model
+        return (f"Driver {driver_name} changed his car from "
+                f"{old_model} to {new_model}.")
+
+
+    def _driver_found_by_name(self, driver_name)-> bool | Driver:
         driver = next((d for d in self.drivers if d.name == driver_name), None)
         if driver:
-            return True
+            return driver
         return False
 
-    def _race_found_by_name(self, race_name) -> bool:
+    def _race_found_by_name(self, race_name) -> bool | Race:
         race = next((r for r in self.races if r.name == race_name), None)
         if race:
-            return True
+            return race
+        return False
+
+    def _last_available_car_found_by_type(self, car_type) -> bool | Car:
+        reversed_cars = self.cars[::-1]
+        car = next((c for c in reversed_cars if type(c).__name__ == car_type and not c.is_taken), None)
+        if car:
+            return car
         return False
 
