4308d3923ea4409b659d02d1bf89d5401df25f97
eleonorakuteva <eleonorakuteva@gmail.com>
2025-12-03 11:59:51 +0200

    Update tournament.py

---
+++ handball_tournament/project/tournament.py
@@ -0,0 +1,24 @@
+from project.equipment.base_equipment import BaseEquipment
+from project.teams.base_team import BaseTeam
+
+
+class Tournament:
+
+    def __init__(self, name: str, capacity: int):
+        self.name = name
+        self.capacity = capacity
+        self.equipment: list[BaseEquipment] = []
+        self.teams: list[BaseTeam] = []
+
+    @property
+    def name(self):
+        return self.__name
+
+    @name.setter
+    def name(self, value: str):
+        if not value.isalnum():
+            raise ValueError("Tournament name should contain letters and digits only!")
+        self.__name = value
+
+    def add_equipment(self, equipment_type: str):
+        pass
