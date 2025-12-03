06f0c5b329c74ad6efbc3c6671872ecb9009a429
eleonorakuteva <eleonorakuteva@gmail.com>
2025-12-03 13:47:17 +0200

    Update tournament.py

--- handball_tournament/project/tournament.py
+++ handball_tournament/project/tournament.py
@@ -36,7 +36,7 @@ class Tournament:
 
     def add_equipment(self, equipment_type: str):
         try:
-            new_equipment = self.VALID_EQUIPMENT_TYPES[equipment_type]
+            new_equipment = self.VALID_EQUIPMENT_TYPES[equipment_type]()
         except KeyError:
             raise Exception("Invalid equipment type!")
 
@@ -58,25 +58,80 @@ class Tournament:
         return f"{team_type} was successfully added."
 
     def sell_equipment(self, equipment_type: str, team_name: str):
-        pass
+        # TODO:	Take the last equipment of the given type from the collection.
+
+        equipment: BaseEquipment = self.VALID_EQUIPMENT_TYPES[equipment_type]()
+        self.equipment.append(equipment)
+
+
+        team = self._find_team_by_name(team_name)
+        if not self._is_team_have_enough_budget(team, equipment.price):
+            raise Exception("Budget is not enough!")
+
+        self.equipment.remove(equipment)
+        team.equipment.append(equipment)
+        team.budget -= equipment.price
+        return f"Successfully sold {equipment_type} to {team_name}."
+
+    @staticmethod
+    def _is_team_have_enough_budget(team: BaseTeam, amount) -> bool:
+        if team.budget >= amount:
+            return True
+        return False
 
     def remove_team(self, team_name: str):
-        pass
+        team = self._find_team_by_name(team_name)
+        if team is None:
+            raise Exception("No such team!")
+        if team.wins >= 1:
+            raise Exception(f"The team has {team.wins} wins! Removal is impossible!")
+
+        self.teams.remove(team)
 
     def increase_equipment_price(self, equipment_type: str):
-        pass
+        if self._find_equipment_by_type(equipment_type) is not None:
+            [e.increase_price() for e in self.equipment if type(e).__name__ == equipment_type]
+
 
     def play(self, team_name1: str, team_name2: str):
-        pass
+        team_1 = self._find_team_by_name(team_name1)
+        team_2 = self._find_team_by_name(team_name2)
+
+        if team_1.type != team_2.type:
+            raise Exception("Game cannot start! Team types mismatch!")
+
+        return self._calculate_winner(team_1, team_2)
+
+    @staticmethod
+    def _calculate_winner(team1, team2):
+        if team1.result_points_advantage_and_protection() > team2.result_points_advantage_and_protection():
+            team1.win()
+            return f"The winner is {team1.name}."
+        elif team2.result_points_advantage_and_protection() > team1.result_points_advantage_and_protection():
+            team2.win()
+            return f"The winner is {team2.name}."
+        elif team1.result_points_advantage_and_protection() == team2.result_points_advantage_and_protection():
+            return f"No winner in this game."
+
 
     def get_statistics(self):
-        pass
+        result = [f"Tournament: {self.name}",
+                  f"Number of Teams: {len(self.teams)}",
+                  "Teams:"]
+
+        sorted_teams = sorted(self.teams, key=lambda t: -t.wins)
+        for t in sorted_teams:
+            result.append(t.get_statistics())
+
+        return '\n'.join(result)
+
 
     def _find_team_by_name(self, team_name):
         return next((t for t in self.teams if t.name == team_name), None)
 
     def _find_equipment_by_type(self, equipment_type):
-        return next(e for e in self.equipment if type(e).__name__ == equipment_type)
+        equipment = next((e for e in self.equipment if type(e).__name__ == equipment_type), None)
+        return equipment
 
 
 
